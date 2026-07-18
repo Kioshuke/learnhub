1ceb2ecbf1ae6a3fd24f71a2e363e4502d8a6061
Kioshuke <quanhao6788@gmail.com>
2026-07-18 12:58:35 +0700

    update 18/07/2026 12:58:35,73

--- push.bat
+++ push.bat
@@ -1,6 +1,5 @@
 @echo off
 git add .
-git commit -m "update dinh ki learnhub"
-git push origin main --force
-echo ---- DEPLOY THANH CONG! ----
+git commit -m "update %date% %time%"
+git push origin main
 pause
